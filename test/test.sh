baed1c0cbbf764e2045f1a0eef7a2e3cd5cf01c7
Ulysse Rubens <ulyrubens@hotmail.com>
2019-06-26 17:31:48 +0200

    Update test configuration

--- test/test.sh
+++ test/test.sh
@@ -9,12 +9,12 @@ tar -cvf v$VERSION.tar.gz ../
 cp v$VERSION.tar.gz ../docker/v$VERSION.tar.gz
 docker build --build-arg FROM_NAMESPACE=cytomineuliege --build-arg FROM_VERSION=v1.2.0 --build-arg RELEASE_PATH="." --build-arg VERSION=$VERSION -t iip-cyto-test ../docker
 
-docker create --name iipCyto \
+docker create --name iipCytoTest \
 -v /data/images:/data/images \
 --restart=unless-stopped \
--p 80:80 \
+-p 8888:80 \
 iip-cyto-test
 
-docker cp nginx.conf.sample iipCyto:/tmp/nginx.conf.sample
-docker cp iip-configuration.sh iipCyto:/tmp/iip-configuration.sh
-docker start iipCyto
+docker cp nginx.conf.sample iipCytoTest:/tmp/nginx.conf.sample
+docker cp iip-configuration.sh iipCytoTest:/tmp/iip-configuration.sh
+docker start iipCytoTest
